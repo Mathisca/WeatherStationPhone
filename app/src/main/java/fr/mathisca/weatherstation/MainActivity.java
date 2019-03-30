3f85478065b25a22b223fd7a38d8f6a1ed377579
Mathis Cariou <contact@mathisca.fr>
2019-03-30 13:39:09 +0100

    Final commit

--- app/src/main/java/fr/mathisca/weatherstation/MainActivity.java
+++ app/src/main/java/fr/mathisca/weatherstation/MainActivity.java
@@ -1,62 +1,53 @@
 package fr.mathisca.weatherstation;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import com.google.android.gms.ads.reward.RewardItem;
-import com.google.android.gms.ads.reward.RewardedVideoAd;
-import com.google.android.gms.ads.reward.RewardedVideoAdListener;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
 
 public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener {
 
     private static final String TAG = "MainActivity";
+
     private RewardedVideoAd mRewardedVideoAd;
+    private WebView wv;
+    private boolean isBlack = true;
+    private Button mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                        if (!task.isSuccessful()) {
-                            return;
-                        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
-                        String token = task.getResult().getToken();
-                        String msg = getString(R.string.fcm_token, token);
-                        Log.d(TAG, msg);
-
-                    }
-                });
+        wv = findViewById(R.id.theweb);
+        mode = findViewById(R.id.darkModeButton);
 
         MobileAds.initialize(this, "ca-app-pub-3578714154155017~7618688036");
+
         mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this);
         mRewardedVideoAd.setRewardedVideoAdListener(this);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        AdView adview = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
 
-        setContentView(R.layout.activity_main);
+        adview.loadAd(adRequest);
 
-        WebView wv = findViewById(R.id.theweb);
+        loadRewardedVideoAd();
 
         wv.setWebViewClient(new WebViewClient() {
             @Override
@@ -68,22 +59,22 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
 
         WebSettings webSettings = wv.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
         wv.loadUrl("https://stationmeteo.node-lab.xyz/");
 
-       AdView adview = findViewById(R.id.adView);
-       AdRequest adRequest = new AdRequest.Builder().build();
-
-       adview.loadAd(adRequest);
-
-       //loadRewardedVideoAd();
     }
 
 
     private void loadRewardedVideoAd() {
-        mRewardedVideoAd.loadAd("ca-app-pub-3578714154155017/6982309505",
+        mRewardedVideoAd.loadAd("ca-app-pub-3578714154155017/6982309505", // Prod
                 new AdRequest.Builder().build());
+//        mRewardedVideoAd.loadAd("ca-app-pub-3940256099942544/5224354917",
+//                new AdRequest.Builder().build());
+
     }
 
     public void showAd(View v) {
@@ -91,14 +82,10 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
             mRewardedVideoAd.show();
         }
     }
-    private boolean showed = false;
 
     @Override
     public void onRewardedVideoAdLoaded() {
-        if(!showed) {
-            mRewardedVideoAd.show();
-            showed = true;
-        }
+        mode.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -113,13 +100,21 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
 
     @Override
     public void onRewardedVideoAdClosed() {
+        mode.setVisibility(View.INVISIBLE);
         loadRewardedVideoAd();
-
     }
 
     @Override
     public void onRewarded(RewardItem rewardItem) {
+        if (isBlack) {
+            wv.loadUrl("https://stationmeteo.node-lab.xyz/?theme=black");
+            mode.setText(getString(R.string.brightmode));
+        } else {
+            wv.loadUrl("https://stationmeteo.node-lab.xyz/");
+            mode.setText(getString(R.string.blackmode));
+        }
 
+        isBlack = !isBlack;
     }
 
     @Override
@@ -136,7 +131,6 @@ public class MainActivity extends AppCompatActivity implements RewardedVideoAdLi
     public void onRewardedVideoCompleted() {
 
     }
-
 
 
     @Override
